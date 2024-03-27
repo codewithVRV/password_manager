74ac4c124b9256025bbbac797d4d82907eb7db84
Vishnu Kumar <vermaji1891@gmail.com>
2024-03-27 20:00:43 +0530

    added single detail modal

--- src/Components/Form Modal/Modal.jsx
+++ src/Components/Form Modal/Modal.jsx
@@ -1,82 +1,93 @@
-function ModalBox (props) {
+import  { useState } from "react";
 
-    console.log("props is", props)
+function ModalBox(props) {
+    const [isOpen, setIsOpen] = useState(true);
+
+    const closeModal = () => {
+        setIsOpen(false);
+    };
 
     return (
-        // <>
-            
-        //     <button data-modal-target="authentication-modal" data-modal-toggle="authentication-modal" className="block text-white flex justify-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
-        //         Click Me
-        //     </button>
-
-        //     <div id="authentication-modal" tabIndex="-1" aria-hidden="true" className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-        //         <div className="relative p-4 w-full max-w-md max-h-full">
-        //             <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-        //                 <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-        //                     <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-        //                         Your Credentials are below.
-        //                     </h3>
-        //                     <button type="button" className="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="authentication-modal">
-        //                         <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-        //                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-        //                         </svg>
-        //                         <span className="sr-only">Close modal</span>
-        //                     </button>
-        //                 </div>
-        //                 <div className="p-4 md:p-5">
-        //                     <form className="space-y-4" action="#">
-        //                         <div>
-        //                             <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Site Name</label>
-        //                             <input type="email" value={props.data.site} name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder=""  />
-        //                         </div>
-        //                         <div>
-        //                             <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
-        //                             <input type="text" value={props.data.username} name="password" id="password" placeholder="" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"  />
-        //                         </div>
-        //                         <div>
-        //                             <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-        //                             <input type="text" value={props.data.password} name="password" id="password" placeholder="" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"  />
-        //                         </div>
-        //                     </form>
-        //                 </div>
-        //             </div>
-        //         </div>
-        //     </div> 
-
-        // </>
         <>
-            <section className="bg-gray-50 dark:bg-gray-900">
-                <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-
-                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                            Your Credentials
-                        </h1>
-                        <form className="space-y-4 md:space-y-6" action="#">
-                        <div>
-                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
-                                <input type="email" value={props.data.site} name="password" id="password" placeholder="" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+            {isOpen && (
+                <section className="fixed top-0 left-0 z-50 w-full h-full flex items-center justify-center bg-gray-500 bg-opacity-50">
+                    <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                                <div className="flex justify-center gap-8">
+                                <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                                    Your Credentials
+                                </h1>
+                                <button
+                                    onClick={closeModal}
+                                    type="button"
+                                >
+                                    <svg
+                                        className="w-3 h-3"
+                                        aria-hidden="true"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 14 14"
+                                    >
+                                        <path
+                                            stroke="currentColor"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="2"
+                                            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                                        />
+                                    </svg>
+                                    <span className="sr-only">Close modal</span>
+                                </button>
+                                </div>
+                                <form className="space-y-4 md:space-y-6" action="#">
+                                    <div>
+                                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                            Sitename
+                                        </label>
+                                        <input
+                                            type="email"
+                                            value={props.data.site}
+                                            name="password"
+                                            id="password"
+                                            placeholder=""
+                                            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        />
+                                    </div>
+                                    <div>
+                                        <label htmlFor="text" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                            Username
+                                        </label>
+                                        <input
+                                            type="text"
+                                            value={props.data.username}
+                                            name="password"
+                                            id="password"
+                                            placeholder=""
+                                            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        />
+                                    </div>
+                                    <div>
+                                        <label htmlFor="text" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                            Confirm password
+                                        </label>
+                                        <input
+                                            type="text"
+                                            value={props.data.password}
+                                            name="confirm-password"
+                                            id="confirm-password"
+                                            placeholder="••••••••"
+                                            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        />
+                                    </div>
+                                </form>
                             </div>
-                            <div>
-                                <label htmlFor="text" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
-                                <input type="text" value={props.data.username} name="password" id="password" placeholder="" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-                            </div>
-                            <div>
-                                <label htmlFor="text" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-                                <input type="text" value={props.data.password} name="confirm-password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
-                            </div>
-
-                        </form>
+                        </div>
                     </div>
-                </div>
-                </div>
-            </section>
+                </section>
+            )}
         </>
-    )
-
-    
+    );
 }
-
 
 export default ModalBox;
